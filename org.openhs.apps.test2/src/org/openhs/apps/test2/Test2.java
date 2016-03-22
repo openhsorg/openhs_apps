0e85d1141c4bc4b9cc6871612f9a884c05a46bba
E454551 <E454551@CZ09LTG93MNX1.global.ds.honeywell.com>
2016-03-22 17:58:13 +0100

    Update

---
+++ org.openhs.apps.test2/src/org/openhs/apps/test2/Test2.java
@@ -0,0 +1,14 @@
+package org.openhs.apps.test2;
+
+public class Test2 {
+	
+    public void activate() {
+    	System.out.println("Component test2 activated!!!");
+
+    }	
+    
+    public void deactivate() {
+
+    }	    
+
+}
