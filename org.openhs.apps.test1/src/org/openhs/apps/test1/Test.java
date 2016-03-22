a1e87815e43b821853c9d46937c1df9a2fd95ead
E454551 <E454551@CZ09LTG93MNX1.global.ds.honeywell.com>
2016-03-22 15:52:34 +0100

    Update

---
+++ org.openhs.apps.test1/src/org/openhs/apps/test1/Test.java
@@ -0,0 +1,14 @@
+package org.openhs.apps.test1;
+
+public class Test {
+
+    public void activate() {
+        System.out.println("Starting Test----->");
+    }
+
+    public void deactivate() {
+        System.out.println("Stopping Test----->");
+    }
+	
+	
+}
