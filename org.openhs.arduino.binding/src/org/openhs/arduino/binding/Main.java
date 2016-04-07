6ce926ab06b47aea38bb5abb77fa98bb22bee183
Michal Valny <michal.valny@openhs.org>
2016-04-07 14:52:52 +0200

    Added Arduino binding.

---
+++ org.openhs.arduino.binding/src/org/openhs/arduino/binding/Main.java
@@ -0,0 +1,13 @@
+package org.openhs.arduino.binding;
+
+public class Main {
+	
+	public void activate() {
+		System.out.println("org.openhs.arduino.binding - activated");
+	}
+	
+	public void deactivate () {
+		System.out.println("org.openhs.arduino.binding - deactivated");
+	}
+
+}
